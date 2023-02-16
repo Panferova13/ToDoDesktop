3bdcbf9fb64e8a90a2ea470fc2e81a5d1c098cbe
Anastasia Panferova <panferovaa@icloud.com>
2023-02-16 22:00:50 +0300

    Inital commit

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using Desktop.Properties;
-
-namespace Desktop
+﻿namespace Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -13,47 +8,6 @@ namespace Desktop
         public MainWindow()
         {
             InitializeComponent();
-            Manager.CurrentWindow = this;
-        }
-        private void Login_btn_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void Registration_btn_Click(object sender, RoutedEventArgs e)
-        {
-            var window = new RegistrationWindow();
-            window.Title = "new Window";
-            window.Show();
-            Manager.CurrentWindow.Hide();
-        }
-
-        private void LoginMailTxb_OnGotFocus(object sender, RoutedEventArgs e)
-        {
-            if (LoginMailTxb.Text != "Введите почту") return;
-            LoginMailTxb.Text = "";
-            LoginMailTxb.Foreground = new SolidColorBrush(Colors.Black);
-        }
-
-        private void LoginMailTxb_OnLostFocus(object sender, RoutedEventArgs e)
-        {
-            if (LoginMailTxb.Text != "") return;
-            LoginMailTxb.Text = "Введите почту";
-            LoginMailTxb.Foreground = new SolidColorBrush(Colors.Gray);
-        }
-
-        private void LoginPassw_txb_OnGotFocus(object sender, RoutedEventArgs e)
-        {
-           if(LoginPassw_txb.Text!="Введите пароль") return;
-           LoginPassw_txb.Text = "";
-           LoginPassw_txb.Foreground = new SolidColorBrush(Colors.Black);
-        }
-
-        private void LoginPassw_txb_OnLostFocus(object sender, RoutedEventArgs e)
-        {
-            if(LoginPassw_txb.Text!="") return;
-            LoginPassw_txb.Text = "Введите пароль";
-            LoginPassw_txb.Foreground = new SolidColorBrush(Colors.Gray);
         }
     }
 }
